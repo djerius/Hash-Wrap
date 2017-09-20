45639ad6e6e08307b1687a2449d26ba618c20b4d
Diab Jerius <djerius@cfa.harvard.edu>
2017-09-20 14:54:05 -0400

    update documentation

--- lib/Return/Object.pm
+++ lib/Return/Object.pm
@@ -96,67 +96,123 @@ __END__
   use Return::Object;
 
   sub foo {
-
-   ...
-
-    return_object \%hash_result;
-
+    return_object { a => 1 };
   }
+
+  $result = foo();
+  print $result->a;  # prints
+  print $result->b;  # throws
 
 =head1 DESCRIPTION
 
-This module provides the L</return_object> subroutine, which
-makes it easier to encapsulate values returned from a subroutine
-as objects.
 
-For hash results, the keys are available as methods, which ensures
-that mistyped keys do not result in auto-vivified elements in the hash.
+This module provides routines which encapsulate a hash as an object.
+The object provides methods for keys in the hash; attempting to access
+a non-existent key via a method will cause an exception.
 
-Result objects can also automatically be made immutable.
+The impetus for this was to encapsulate data returned from a
+subroutine or method (hence the name).  Returning a bare hash can lead
+to bugs if there are typos in hash key names when accessing the hash.
+
+It is not necessary for the hash to be fully populated when the object
+is created.  The underlying hash may be manipulated directly, and
+changes will be reflected in the object's methods.  To prevent this,
+consider using the lock routines in L<Hash::Util> on the object after
+creation.
+
+Only hash keys which are legal method names will be accessible via
+object methods.
+
+=head2 Object construction and constructor customization
+
+By default C<Object::Return> exports a C<return_object> constructor
+which, given a hashref, blesses it directly into the
+B<Return::Object::Class> class.
+
+The constructor may be customized to change which class the object is
+instantiated from, and how it is constructed from the data.
+C<Return::Object> uses L<Exporter::Tiny> to perform the customization.
+For example,
+
+  use Return::Object
+    return_object => { -as => 'return_cloned_object',
+                       -clone => 1 };
+
+will create a version of C<return_object> which clones the passed hash
+and is imported as C<return_cloned_object>.  To import it under
+the original name, C<return_object>, leave out the C<-as> option.
 
 
-=head2 Object construction
+The following options are available to customize the constructor.
 
-By default C<Object::Return> generates a C<return_object> subroutine which
-has the following characteristics:
+=over
 
-=head3 For Hashes
+=item C<-as> => I<subroutine name>
+
+This is optional, and imports the customized version of
+C<return_object> with the given name.
+
+=item C<-class> => I<class name>
+
+The object will be blessed into the specified class.  If the class
+should be created on the fly, specify the C<-create> option.
+See L</Object Classes> for what is expected of the object classes.
+This defaults to C<Object::Return::Class>.
+
+=item C<-create> => I<boolean>
+
+If true, and C<-class> is specified, a class with the given name
+will be created.
+
+=item C<-copy> => I<boolean>
+
+If true, the object will store the data in a I<shallow> copy of the
+hash. By default, the object uses the hash directly.
+
+=item C<-clone> => I<boolean>
+
+If true, the object will store the data in a deep copy of the hash,
+made with L<Storeable/dclone>. By default, the object uses the hash
+directly.
+
+
+=back
+
+=head2 Object Classes
+
+An object class has the following properties:
 
 =over
 
 =item *
 
-Hash elements may be added or deleted directly from the underlying hash
+The class must be a subclass of C<Return::Object::Base>.
 
 =item *
 
-Hash keys are available as object methods.  Keys which do not have
-valid method names are translated (see L</Key Translation>).
+The class typically does not provide any methods, as they would mask
+a hash key of the same name.
+
+=item *
+
+The class need not have a constructor.  If it does, it is passed a
+hashref which it should bless as the actual object.  For example:
+
+  package My::Result;
+  use parent 'Return::Object::Base';
+
+  sub new {
+    my  ( $class, $hash ) = @_;
+    return bless $hash, $class;
+  }
+
+This excludes having a hash key named C<new>.
 
 =back
 
-=head3 For Arrays
-
-=over
-
-=item *
-
-Hash elements may be added or deleted directly from the underlying hash
-
-=item *
-
-Hash keys are available as object methods.  Keys which do not have
-valid method names are translated (see L</Key Translation>).
-
-
-
-=back
-
-
-=head3 Hashes
-
-Hash return objects may have the
-
+C<Return::Object::Base> provides an empty C<DESTROY> method, a
+C<can> method, and an C<AUTOLOAD> method.  They will mask hash
+keys with the same names.
 
 
 =head1 SEE ALSO
